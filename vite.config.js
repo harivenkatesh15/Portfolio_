8b45d7fc5fdd136b4421ab7e741b4650ecf1e0f4
Hari <harivenkatesh1505@gmail.com>
2026-03-29 23:25:57 +0530

    changed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio_/',  // exact repo name on GitHub
+  base: '/',  // exact repo name on GitHub
   plugins: [react()],
 })
